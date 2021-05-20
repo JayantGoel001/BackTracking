e35f377f0592470a5daf53e3de61e395dd4382c1
JayantGoel001 <jgoel92@gmail.com>
2021-05-20 15:09:35 +0530

    Initial Commit

--- HamiltonCycle.cpp
+++ HamiltonCycle.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 using namespace std;
-void printSolution(int *path,int n)
-{
+void printSolution(int *path,int n){
     cout<<"\nThe Hamilton Path of Graph is:\n";
     for(int i=0;i<n;i++)
     {
@@ -9,47 +8,31 @@ void printSolution(int *path,int n)
     }
     cout<<path[0];
 }
-bool isSafe(int **graph,int n,int v,int *path,int pos)
-{
-    if(graph[path[pos-1]][v]==0)
-    {
+bool isSafe(int **graph,int n,int v,int *path,int pos){
+    if(graph[path[pos-1]][v]==0){
         return false;
     }
-    for(int i=0;i<pos;i++)
-    {
-        if( path[i]==v)
-        {
+    for(int i=0;i<pos;i++){
+        if( path[i]==v){
             return false;
         }
     }
     return true;
 }
-bool hamCycleUtil(int **graph,int n,int *path,int pos)
-{
-    if(pos==n)
-    {
-        if(graph[path[pos-1]][path[0]]==1)
-        {
+bool hamCycleUtil(int **graph,int n,int *path,int pos){
+    if(pos==n){
+        if(graph[path[pos-1]][path[0]]==1){
             return true;
-        }
-        else
-        {
+        }else{
             return false;
         }
-    }
-    else
-    {
-        for(int i=1;i<n;i++)
-        {
-            if(isSafe(graph,n,i,path,pos)==true)
-            {
+    }else{
+        for(int i=1;i<n;i++){
+            if(isSafe(graph,n,i,path,pos)){
                 path[pos]=i;
-                if(hamCycleUtil(graph,n,path,pos+1)==true)
-                {
+                if(hamCycleUtil(graph,n,path,pos+1)){
                     return true;
-                }
-                else
-                {
+                }else{
                     path[pos]=-1;
                 }
             }
@@ -57,38 +40,29 @@ bool hamCycleUtil(int **graph,int n,int *path,int pos)
         return false;
     }
 }
-void hamCycle(int **graph,int n)
-{
+void hamCycle(int **graph,int n){
     int *path=new int[n];
-    for(int i=0;i<n;i++)
-    {
+    for(int i=0;i<n;i++){
         path[i]=-1;
     }
     path[0]=0;
-    if(hamCycleUtil(graph,n,path,1)==true)
-    {
+    if(hamCycleUtil(graph,n,path,1)){
         printSolution(path,n);
-    }
-    else
-    {
+    }else{
         cout<<"Solution does not exists.";
     }
 }
-int main()
-{
+int main(){
     int n;
     cout<<"Enter the value of n:\n";
     cin>>n;
     int **graph=new int*[n];
-    for(int i=0;i<n;i++)
-    {
+    for(int i=0;i<n;i++){
         graph[i]=new int[n];
     }
     cout<<"\n\nEnter the elements of array:\n";
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
             cin>>graph[i][j];
         }
     }
