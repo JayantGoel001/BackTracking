106b92793fc3e59c3cd6690e0aa10093019fb6bf
JayantGoel001 <jgoel92@gmail.com>
2021-05-20 15:07:18 +0530

    Initial Commit

--- AllBit.cpp
+++ AllBit.cpp
@@ -1,38 +1,30 @@
 #include<iostream>
+#define int long long int
 using namespace std;
-void printSolution(int *ar,int n)
-{
-    for(int i=0;i<n;i++)
-    {
+void printSolution(int *ar,int n){
+    for(int i=0;i<n;i++){
         cout<<ar[i]<<" ";
     }
     cout<<endl;
 }
-void AllBinaryCodeUtil(int *ar,int n,int i)
-{
-    if(i==n)
-    {
+void AllBinaryCodeUtil(int *ar,int n,int i){
+    if(i==n){
         printSolution(ar,n);
-    }
-    else
-    {
+    }else{
         ar[i]=0;
         AllBinaryCodeUtil(ar,n,i+1);
         ar[i]=1;
         AllBinaryCodeUtil(ar,n,i+1);
     }
 }
-void AllBinaryCode(int n)
-{
+void AllBinaryCode(int n){
     int *ar=new int[n];
-    for(int i=0;i<n;i++)
-    {
+    for(int i=0;i<n;i++){
         ar[i]=0;
     }
     AllBinaryCodeUtil(ar,n,0);
 }
-int main()
-{
+int32_t main(){
     int n;
     cout<<"Enter the value of n:\n";
     cin>>n;
